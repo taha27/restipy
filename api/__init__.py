83d5da08a9daa7fecd631401cc3bac5fce7d1a3c
taha27 <tahashabbir27@gmail.com>
2017-07-13 00:39:44 -0400

    starting work to store in and read from mongodb server

--- api/__init__.py
+++ api/__init__.py
@@ -2,15 +2,38 @@
 Create and configure a Flask server
 """
 from flask import Flask
+from pymongo import MongoClient
 
 app = Flask(__name__)
 
+def get_db():
+    client = MongoClient('localhost', 27017)
+    companies_db = client.companies_database
+    return companies_db
+
 # Fake company database
+# def database():
+#     """
+#     Represents and returns a datastore of company information
+#     """
+#     return {
+#         "exponential": {
+#             "name": "Exponential.io",
+#             "city": "Palo Alto"
+#         },
+#         "ibm": {
+#             "name": "International Business Machines",
+#             "city": "New York"
+#         }
+#     }
+
 def database():
     """
-    Represents and returns a datastore of company information
+    Get database info from mongodb test database
     """
-    return {
+    db = get_db()
+
+    sample_company_data = {
         "exponential": {
             "name": "Exponential.io",
             "city": "Palo Alto"
@@ -21,6 +44,12 @@ def database():
         }
     }
 
-# Routes
+    for key, value in sample_company_data.items():
+        db.companies.insert({key: value})
+
+    # return db.test.find
+
+# Routes stored in CRUD pattern
+from api.companies import create_company
 from api.companies import read_company
 from api.companies import read_companies
