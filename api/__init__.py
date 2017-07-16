2ddf411f5905a0a9c6f8b67fed3b74aec09fd905
taha27 <tahashabbir27@gmail.com>
2017-07-16 01:54:32 -0400

    added env var fetch for mongodb connection uri

--- api/__init__.py
+++ api/__init__.py
@@ -1,36 +1,36 @@
 """
 Create and configure a Flask server
 """
+import os
+import sys
 from pymongo import MongoClient
 from flask import Flask
 
-app = Flask(__name__)
+# Check if the MongoDB Connection URI string is set as an environment variable
+DB_URI = os.environ.get('RESTIPY_DB_URI')
+if DB_URI is None:
+    sys.exit('The "RESTIPY_DB_URI" environment variable could not be found.'\
+             ' Please set it using the following standard URI connection scheme:\n'\
+             'mongodb://[username:password@]host1[:port1][,host2[:port2],...[,hostN[:portN]]]'\
+             '[/[database][?options]]')
+    # Comment out the sys.exit(...) above and uncomment the line
+    # below to connect on default mongod host and port
+    # DB_URI = 'mongodb://localhost:27017/'
 
-# Fake company database
-def database_dummy():
-    """
-    Represents and returns a datastore of company information
-    """
-    return {
-        "exponential": {
-            "name": "Exponential.io",
-            "city": "Palo Alto"
-        },
-        "ibm": {
-            "name": "International Business Machines",
-            "city": "New York"
-        }
-    }
+
+# Create an instance of a
+app = Flask(__name__)
 
 def database():
     """
-    Represents and returns a datastore of company information
+    Returns a datastore of company information
     """
-    client = MongoClient('mongodb://localhost:27017/')
+    client = MongoClient(DB_URI)
     db = client.restipy
     companies = db.companies
     return companies
 
 # Routes
+from api.companies import create_company
 from api.companies import read_company
 from api.companies import read_companies
--- server.py
+++ server.py
@@ -1,6 +1,7 @@
 """
 Entry point for this custom API server
 """
-
 from api import app
-app.run(debug=True)
+
+if __name__ == '__main__':
+    app.run(debug=True)
